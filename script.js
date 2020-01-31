cc4debb192b8a45a88cc69d815f080f895a7be25
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-02-01 00:17:32 +0100

    randomly generated buttons

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-let dzwieki = ['./sounds/Familiada/dobrze.mp3', './sounds/Familiada/blad.mp3', 'sounds/1z10/dobrze.mp3', 'sounds/1z10/zle.mp3'];
+const kolory = ['red', 'green', 'blue', 'purple', 'teal'];
+
+const text = ['Familiada - dobrze', 'Familiada - źle', 'Familiada - było', '1 z 10 - dobrze', '1 z 10 - źle', '1 z 10 - otóż nie'];
+
+const body = document.querySelector('body');
+
+function randomColor() {
+	return kolory[Math.floor(Math.random() * kolory.length)];
+}
+
+function generujPrzyciski() {
+	for (let i = 0; i < text.length; i++) {
+		let div = document.createElement('div');
+		div.setAttribute('id', i);
+		div.setAttribute('class', randomColor());
+		let span = document.createElement('span');
+		span.innerText = text[i];
+		div.appendChild(span);
+		body.appendChild(div);
+	}	
+}
+
+generujPrzyciski();
+
+let dzwieki = ['./sounds/Familiada/dobrze.mp3', './sounds/Familiada/blad.mp3', 'sounds/Familiada/bylo.m4a', 'sounds/1z10/dobrze.mp3', 'sounds/1z10/zle.mp3'];
 
 for (let i = 0; i < dzwieki.length; i++) {
 	dzwieki[i] = new Audio(dzwieki[i]);

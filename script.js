0c9753cf4e5a680cfde351f9890ceb02769d7137
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-01-31 23:43:40 +0100

    working script for playing audio

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let dzwieki = ['familiada-dobrze', 'familiada-zle', '1z10-dobrze', '1z10-zle'];
+let dzwieki = ['./sounds/Familiada/dobrze.mp3', './sounds/Familiada/blad.mp3', 'sounds/1z10/dobrze.mp3', 'sounds/1z10/zle.mp3'];
 
+for (let i = 0; i < dzwieki.length; i++) {
+	dzwieki[i] = new Audio(dzwieki[i]);
+}
+
+function dzwiek(id) {
+	dzwieki[id].play();
+}
+
+let div = document.querySelectorAll('body > div');
+
+for (let i = 0; i < div.length; i++) {
+	div[i].addEventListener('click', function() {
+		dzwiek(div[i].getAttribute('id'));
+	});
+}

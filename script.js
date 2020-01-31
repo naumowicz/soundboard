b78ce7e9c4f1d81a5e11400bc5a64c186eced703
Mateusz Naumowicz <naumowicz@protonmail.com>
2020-02-01 00:21:01 +0100

    optymalization

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const kolory = ['red', 'green', 'blue', 'purple', 'teal'];
 
 const text = ['Familiada - dobrze', 'Familiada - źle', 'Familiada - było', '1 z 10 - dobrze', '1 z 10 - źle', '1 z 10 - otóż nie'];
 
+let dzwieki = ['./sounds/Familiada/dobrze.mp3', './sounds/Familiada/blad.mp3', 'sounds/Familiada/bylo.m4a', 'sounds/1z10/dobrze.mp3', 'sounds/1z10/zle.mp3', 'sounds/1z10/otoznie.m4a'];
+
+for (let i = 0; i < dzwieki.length; i++) {
+	dzwieki[i] = new Audio(dzwieki[i]);
+}
+
+function dzwiek(id) {
+	dzwieki[id].play();
+}
+
 const body = document.querySelector('body');
 
 function randomColor() {
@@ -15,27 +25,12 @@ function generujPrzyciski() {
 		div.setAttribute('class', randomColor());
 		let span = document.createElement('span');
 		span.innerText = text[i];
+		div.addEventListener('click', function() {
+			dzwiek(i);
+		});
 		div.appendChild(span);
 		body.appendChild(div);
 	}	
 }
 
 generujPrzyciski();
-
-let dzwieki = ['./sounds/Familiada/dobrze.mp3', './sounds/Familiada/blad.mp3', 'sounds/Familiada/bylo.m4a', 'sounds/1z10/dobrze.mp3', 'sounds/1z10/zle.mp3'];
-
-for (let i = 0; i < dzwieki.length; i++) {
-	dzwieki[i] = new Audio(dzwieki[i]);
-}
-
-function dzwiek(id) {
-	dzwieki[id].play();
-}
-
-let div = document.querySelectorAll('body > div');
-
-for (let i = 0; i < div.length; i++) {
-	div[i].addEventListener('click', function() {
-		dzwiek(div[i].getAttribute('id'));
-	});
-}
